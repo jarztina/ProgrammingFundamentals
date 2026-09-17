d929f115109983f54a4dec936e028f3bc10f2471
jarztina <2601738@sit.singaporetech.edu.sg>
2026-09-18 00:08:15 +0800

    Initial Commit

---
+++ modular_auditor.py
@@ -0,0 +1,11 @@
+inventory = 0
+
+def get_valid_input(choice):
+    if choice == 'quit':
+        print("GoodBye!")
+        
+
+
+while True:
+    choice = input("Enter Stock Quantity")
+    get_valid_input(choice)
