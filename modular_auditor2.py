3e24831e4a934c227e7d12fd4ab27f2cb8e773d6
jarztina <2601738@sit.singaporetech.edu.sg>
2026-09-18 11:50:30 +0800

    Calculate Tax

--- modular_auditor2.py
+++ modular_auditor2.py
@@ -1,5 +1,6 @@
 inventory = 0
 failed_attempts = 0
+processed_delivery = 0
 
 def get_valid_input():
     stock = input("Enter the stock quantity: ")
@@ -9,15 +10,30 @@ def get_valid_input():
         print("Please enter a valid stock quantity")
         return None
     else:
-        return stock
+        return int(stock)
 
+def process_delivery(current_total, newStock):
+    new_total = current_total + newStock
+    return new_total
+
+def calculate_tax(newStock):
+    tax = newStock * 0.1
+    return tax
+
+def generate_report(deliveryProcessed, failedAttempts):
+    print(f"Total Delivery Processed: {deliveryProcessed}")
+    print(f"Total Failed Attemps: {failedAttempts}")
 
 while True:
     stock = get_valid_input()
     if stock == 'quit':
+        generate_report(inventory, failed_attempts)
         print("GoodBye!")
     if stock is None:
         failed_attempts += 1
         continue
-    process_delivery(inventory, stock)
-    
+    inventory = process_delivery(inventory, stock)
+    tax = calculate_tax
+    print(f"Tax for this delivery: {tax}")
+    processed_delivery += 1
+
